6dfddde1dabcc1e3bbe49db9b01b09f7702550c7
lukas <lukas@LukasDesktop.mshome.net>
2023-02-10 22:02:53 -0800

    did more stuff

--- RPG/src/mainpack/Enemy.java
+++ RPG/src/mainpack/Enemy.java
@@ -12,41 +12,65 @@ public class Enemy {
 	private int at;
 	private int df;
 	private GridBagConstraints gridBag;
+	private JTextField healthBox;
+	private JTextField nameBox;
+	private JTextField attackBox;
+	private JPanel panel;
 	private static int numEnemies = 0;
 	
 	public Enemy(String name, int health, int speed, int attack, int defense)
 	{
+
 		this.name = name;
 		hp = health;
 		spd = speed;
 		at = attack;
 		df = defense;
 		numEnemies += 1;
+		System.out.println(numEnemies);
+		setPanel();
 	}
 	
-	public JPanel getPanel() {
-		JPanel product = new JPanel(new GridBagLayout());
-		JTextField nameBox = new JTextField(name);
-		JTextField healthBox = new JTextField("hp" + hp);
-		JTextField attackBox = new JTextField("at" + at);
+	private void setPanel() {
+		panel = new JPanel(new GridBagLayout());
+		nameBox = new JTextField(name);
+		nameBox.setEditable(false);
+		nameBox.setHorizontalAlignment(JTextField.CENTER);
+		healthBox = new JTextField(" hp: " + hp);
+		healthBox.setEditable(false);
+		healthBox.setHorizontalAlignment(JTextField.CENTER);
+		attackBox = new JTextField(" at: " + at);
+		attackBox.setEditable(false);
+		attackBox.setHorizontalAlignment(JTextField.CENTER);
 		GridBagConstraints gridBag = new GridBagConstraints();
 		
 		gridBag.fill = GridBagConstraints.HORIZONTAL;
 		gridBag.gridy = 0;
 		gridBag.gridwidth = 2;
-		product.add(nameBox,gridBag);
+		panel.add(nameBox,gridBag);
 		gridBag.gridwidth = 1;
 		gridBag.gridy = 2;
 		gridBag.gridx = 0;
-		product.add(healthBox,gridBag);
+		panel.add(healthBox,gridBag);
 		gridBag.gridx = 1;
-		product.add(attackBox,gridBag);
-		
+		panel.add(attackBox,gridBag);
+	}
+	
+	public JPanel getPanel()
+	{
+		return panel;
+	}
+	public GridBagConstraints getBag(int x) {
+		GridBagConstraints product = new GridBagConstraints();
+		product.gridx = x;
 		return product;
 	}
-	public GridBagConstraints getBag() {
-		GridBagConstraints product = new GridBagConstraints();
-		product.gridx = numEnemies - 1;
-		return product;
+	public void doDamage(int damage)
+	{
+		if ((hp - damage) <= 0)
+			hp = 0;
+		else
+			hp -= damage;
+		healthBox.setText(" hp: " + hp);
 	}
 }
--- RPG/src/mainpack/RpgGui.java
+++ RPG/src/mainpack/RpgGui.java
@@ -1,6 +1,7 @@
 package mainpack;
 
 import java.awt.*;
+import java.util.concurrent.TimeUnit;
 
 import javax.swing.*;
 
@@ -10,11 +11,22 @@ public class RpgGui {
 	private JFrame frame;
 	private JPanel panel;
 	private JPanel field;
-	private JTextField topText;
+	private JPanel field2;
+	private JPanel menu;
 	private GridBagConstraints textBag;
 	private GridBagConstraints fieldBag;
-	private GridBagConstraints enemyBag;
+	private GridBagConstraints fieldBag2;
+	private GridBagConstraints menuBag;
+	private GridBagConstraints menuSubBag;
+	private JTextField topText;
+	private JTextArea menuText;
+	private JButton movesButton;
+	private JButton bagButton;
+	private JButton setButton;
+
 	private Enemy enemy1;
+	private Enemy enemy2;
+	private Enemy monster1;
 	
 	
 	public RpgGui() {
@@ -22,36 +34,74 @@ public class RpgGui {
 		// constructing variables
 		frame = new JFrame();
 		panel = new JPanel(new GridBagLayout());
-		
-		topText = new JTextField("Lorem Ipsum");
-		
+		menu = new JPanel(new GridBagLayout());
 		field = new JPanel(new GridBagLayout());
-		
+		field2 = new JPanel(new GridBagLayout());
+		topText = new JTextField("Lorem Ipsum");
+		menuText = new JTextArea();
+		menuBag = new GridBagConstraints();
 		textBag = new GridBagConstraints();
 		fieldBag = new GridBagConstraints();
-		enemyBag = new GridBagConstraints();
-		 
+		fieldBag2 = new GridBagConstraints();
+		menuSubBag = new GridBagConstraints();
+		movesButton = new JButton("Moves");
+		bagButton = new JButton("Bag");
+		setButton = new JButton("settings");
 		
 		// config topText gridbagconstraints
-		// fills horizontal axis when needed
 		textBag.fill = GridBagConstraints.HORIZONTAL;
-		// position x = 0
 		textBag.gridy = 0;
-		
-		// config enemy1
-		enemy1 = new Enemy("test",10,10,10,10);
-		
-		
-		field.add(enemy1.getPanel(),enemy1.getBag());
-		// setting position of field
+		textBag.gridwidth = 3;
+		topText.setHorizontalAlignment(JTextField.CENTER);
+
+		// config fieldBag gridbagconstraints
 		fieldBag.gridy = 1;
+		fieldBag.gridheight = 2;
 		
-		textBag.gridy = 0;
-		// setting panel border
-		panel.setBorder(BorderFactory.createEmptyBorder(20,20,20,20));
-		// adding components
+		// config fieldBag2 gridbagconstraints
+		fieldBag2.gridy = 3;
+		fieldBag2.gridheight = 2;
+		
+		// config menuBag gridbagconstraints
+		menuBag.gridx = 1;
+		menuBag.gridheight = 4;
+		menuBag.gridy = 1;
+		menuBag.weighty = 1;
+		menuBag.fill = GridBagConstraints.BOTH;
+		
+		// config menu
+		menuSubBag.gridx = 0;
+		menuSubBag.gridy = 0;
+		menu.add(movesButton,menuSubBag);
+		menuSubBag.gridx = 1;
+		menu.add(bagButton,menuSubBag);
+		menuSubBag.gridx = 2;
+		menu.add(setButton,menuSubBag);
+		menuSubBag.gridx = 0;
+		menuSubBag.gridy = 1;
+		menuSubBag.gridwidth = 3;
+		menuSubBag.ipady = 40;
+		menuSubBag.fill = GridBagConstraints.BOTH;
+		menu.add(menuText,menuSubBag);
+		
+		
+		// config enemies
+		enemy1 = new Enemy("test", 10, 10, 10, 10);
+		field.add(enemy1.getPanel(),enemy1.getBag(0));
+		enemy2 = new Enemy("test 2", 10, 10, 10, 10);
+		field.add(enemy2.getPanel(),enemy2.getBag(1));
+		
+		// config field2
+		monster1 = new Enemy("mine",10,10,10,10);
+		field2.add(monster1.getPanel(),monster1.getBag(0));
+		
+		
+		// config panel
+		panel.setBorder(BorderFactory.createEmptyBorder(5,5,5,5));
 		panel.add(topText,textBag);
 		panel.add(field,fieldBag);
+		panel.add(field2,fieldBag2);
+		panel.add(menu,menuBag);
 		
 		// config frame
 		frame.add(panel);
@@ -59,6 +109,8 @@ public class RpgGui {
 		frame.setTitle("RPG");
 		frame.pack();
 		frame.setVisible(true);
+
+
 	}
 	
 	
